d9d2e0a91cd96fdba4ead6a9df3f1448437b063e
markup54 <marzenakupka@tlen.pl>
2024-01-09 08:02:43 +0100

    zad2015_z bledem

--- zad2019nowaPR.py
+++ zad2019nowaPR.py
@@ -25,10 +25,12 @@ def suma_silnia(liczba):
         suma = suma + silnie[cyfra]
     return suma
 licznik=0
+wszystkie_liczby =[]
 with open("liczby.txt") as dane:
     for wiersz in dane:
         #print(wiersz)
         liczba= int(wiersz)
+        wszystkie_liczby.append(liczba)
         if liczba in potegi:
             #print("potega",liczba)
             licznik += 1
@@ -38,3 +40,42 @@ with open("liczby.txt") as dane:
             print(liczba)
 print("zad a",licznik)
 print(suma_silnia.__doc__)
+
+print(wszystkie_liczby)
+def nwd (a,b):
+    """
+    odejmowanie -> przy 0 pętla nieskończona
+    :param a:
+    :param b:
+    :return:
+    """
+    while a!=b:
+        if a>b:
+            a=a-b
+        else:
+            b=b-a
+    return a
+
+def nwd_lepiej(a,b):
+    while b!=0:
+        a,b   = b,a%b #operacje na danych spakowanych
+        """
+        reszta = a%b
+        a =b 
+        b = pomoc
+        """
+    return a
+
+print(nwd_lepiej(0,65))
+
+ciag_liczb = [0]
+
+nwd_wartosc = nwd(wszystkie_liczby[0],wszystkie_liczby[1])
+for i in range(2,len(ciag_liczb)):
+    if nwd_wartosc>1:
+        ciag_liczb.append(ciag_liczb[-1]+1)
+        nwd_wartosc = nwd(nwd_wartosc,wszystkie_liczby[i])
+    else:
+        ciag_liczb.append(0)
+        nwd_wartosc = nwd(wszystkie_liczby[i-1],wszystkie_liczby[i])
+print(max(ciag_liczb))
