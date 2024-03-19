50f5a31b5bdecb05bc93123b05075ab2e3893689
markup54 <marzenakupka@tlen.pl>
2024-03-19 08:11:11 +0100

    zad4.2,3 - rok 2019

--- zad2019_PR_wtorek.py
+++ zad2019_PR_wtorek.py
@@ -19,7 +19,39 @@ with open ("liczby.txt") as dane:
         if liczba == 1:
             print(pomoc)
 
+lista_silni_cyfr = [1]
+silnia = 1
+for i in range(1,10):
+    #5 ! = 1*2*3*4*5
+    silnia =silnia*i
+    lista_silni_cyfr.append(silnia)
+print(lista_silni_cyfr)
+
 print("zadanie 4.2")
-with open ("przyklad.txt") as dane:
+with open ("liczby.txt") as dane:
     for wiersz in dane:
+        #suma silni cyfr == liczba
         liczba = int(wiersz)
+        suma =0
+        pomoc = liczba
+        while liczba > 0:
+            cyfra = liczba%10
+            suma = suma +lista_silni_cyfr[cyfra]
+            liczba = liczba//10
+        if pomoc==suma:
+            print(pomoc)
+
+print("zadanie 4.3")
+
+def nwd(a,b):
+    while a !=b:
+        if a>b :
+            a=a-b
+        else:
+            b= b-a
+    return a
+
+liczby_wczytane_z_pliku = []
+with open ("liczby.txt") as dane:
+    for wiersz in dane:
+        liczby_wczytane_z_pliku.append(int(wiersz))
